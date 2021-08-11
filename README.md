eb16a0a04c43306de8dd70edf89873206f01dd23
Paul Neumann <p.neumann76@gmail.com>
2021-08-11 03:41:15 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,44 @@
+WG-Gesucht-Updater-Bot
+=====================
+
+## Installation
+
+First, you need to clone the repository. You need Google Chrome for the script to work, so make sure you have it
+installed.
+
+Second, you need to download [ChromeDriver](http://chromedriver.chromium.org/downloads), if you haven't already. Do make
+sure you get the right version for your version of Chrome, e.g., Chrome version 92.
+
+Third, you'll need selenium so python can automate chrome via ChromeDriver. For this you simply
+type `pip install selenium` into a command prompt and hit enter.
+
+The last thing you'll need to do is rename the `config-sample.ini` file to `config.ini` and change the fields
+accordingly.
+
+## Config
+
+### [account]
+
+Just replace `username` and `password` with your login information.
+
+### [setup]
+
+Here you change the value of `interval` to how often you want your listings to be updated. This value is measured in
+seconds, so, e.g., if you want them to be updated every hour change this to `interval = 3600`
+
+### [chromedriver]
+
+Change the `path` to where you stored the chromedriver.exe on your computer. This is defaulted
+to `C:\Program Files (x86)\chromedriver.exe`. Keep in mind that if you move chromedriver.exe afterwards, you'll have to
+adjust the path again.
+
+### [listings]
+
+Here you can add as many listings as you like. Naming does not affect the code. Simply copy and paste your listing url
+to a variable, e.g., `url1 = https://www.wg-gesucht.de/678910.html?ma=1`. Remove the lines you don't need of course.
+
+## Usage
+
+Simply execute the `wg-gesucht-bot.py` file. You'll see a python terminal that shows some information, e.g., when it last
+updated your listings. You cannot move that file out of the folder, but you can always create a shortcut to your desktop
+or anywhere you like.
---
+++ wg-gesucht-bot.py
@@ -0,0 +1,75 @@
+import time
+from datetime import datetime
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from configparser import ConfigParser
+
+
+# helper functions to check if an element exists
+def check_exists_by_id(element_id):
+    try:
+        driver.find_element_by_id(element_id)
+    except NoSuchElementException:
+        return False
+    return True
+
+
+def check_exists_by_class(class_name):
+    try:
+        driver.find_element_by_class_name(class_name)
+    except NoSuchElementException:
+        return False
+    return True
+
+
+# read the config file
+file = 'config.ini'
+config = ConfigParser()
+config.read(file)
+
+# create a new chromedriver object
+options = webdriver.ChromeOptions()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+driver = webdriver.Chrome(config['chromedriver']['path'], options=options)
+
+print("This program will update your WG-Gesucht listings so they will be listed at the top/as new again.")
+print("For help, visit the github page or read the README.")
+
+driver.get("https://wg-gesucht.de")
+
+# check if the prompt to accept cookies shows up and
+if check_exists_by_id("cmpbntyestxt"):
+    btn = driver.find_element_by_id("cmpbntyestxt").click()
+
+# access login prompt
+driver.find_element_by_xpath('//*[@id="headbar_wrapper"]/div[2]/a[3]').click()
+time.sleep(1)
+
+print("Logging in..")
+
+# login with user data from config file
+username = driver.find_element_by_name("login_email_username")
+password = driver.find_element_by_id("login_password")
+username.send_keys(config['account']['username'])
+password.send_keys(config['account']['password'])
+driver.find_element_by_id("login_submit").click()
+time.sleep(5)
+
+if check_exists_by_class("logout_button"):
+    print("Successfully logged in!")
+
+while True:
+    # iterate through listings
+    for key, url in config.items('listings'):
+        print("Currently updating: {}".format(url))
+        # open listing
+        driver.get(url)
+        # open the edit section
+        driver.find_element_by_xpath('//*[@id="main_column"]/div[2]/div[2]/a').click()
+        time.sleep(3)
+        # and finally update the listing
+        driver.find_element_by_id("update_offer").click()
+        time.sleep(1)
+
+    print("Updated at {}".format(datetime.now().strftime("%Y-%m-%d %H:%M")))
+    time.sleep(float(config['setup']['interval']))
